f781e51153045ec3d306e11b96e1460092c04eed
EC2 Default User <ec2-user@ip-172-31-40-60.ap-northeast-1.compute.internal>
2025-02-07 14:15:52 +0000

    新規登録・ログイン画面のレイアウト整理

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,11 +4,13 @@ class SearchesController < ApplicationController
   def search
     # 検索モデルから情報を受け取っている
     @range = params[:range]
-
+  
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
     else
       @posts = Post.looks(params[:search], params[:word])
     end
+  
+    render "search_result" # search_result.html.erbを表示する
   end
 end

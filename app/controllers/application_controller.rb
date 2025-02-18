87446fd69ccd3bb4dc2492f233271efe40a52ea8
EC2 Default User <ec2-user@ip-172-31-40-60.ap-northeast-1.compute.internal>
2025-02-18 14:49:52 +0000

    scope moduleへ変更　管理者機能ユーザー詳細画面作成開始

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_authentication
  
   private
  
-  def configure_authentication
-    if admin_controller?
-      authenticate_admin!
-    else
-      authenticate_user! unless action_is_public?
-    end
-  end
+      # authenticate_admin!
+
  
   def admin_controller?
     self.class.module_parent_name == 'Admin'
--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,21 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
-
-  namespace :admim do
+  namespace :admin do
     get 'dashboards', to: 'dashboards#index'
     resources :users, only: [:destroy]
   end
-
+  
+  root to: 'public/posts#index'
   scope module: :public do
     devise_for :users
-    root to: "posts#index"
     get 'homes/about', to: 'homes#about', as: :about
     resources :posts do
       resources :comments, only: [:create, :destroy]
     end
     resources :users, only: [:show, :edit, :update]
-  patch 'posts/:id' => 'posts#update', as: 'update_post'
-  get 'search' => 'searches#search'
+    patch 'posts/:id' => 'posts#update', as: 'update_post'
+    get 'search' => 'searches#search'
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
